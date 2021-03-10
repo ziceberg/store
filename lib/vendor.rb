fbab6e8267348a0e03c999e4268f881bcacda8d9
Zachery Bergman <bzach634@gmail.com>
2021-03-10 09:31:58 -0700

    Add vendor methods and tests

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -6,4 +6,15 @@ class Vendor
     @name = name
     @inventory = {}
   end
+
+  def check_stock(item)
+    @inventory.reduce(0) do |acc, item|
+      acc += item[1]
+    end
+  end
+
+  def stock(item, amount)
+    @inventory[item] = 0 if @inventory[item].nil?
+    @inventory[item] += amount
+  end
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/item'
 require './lib/vendor'
+require 'pry'
 
 class VendorTest < Minitest::Test
   def test_it_exists
@@ -13,5 +14,51 @@ class VendorTest < Minitest::Test
     vendor = Vendor.new("Rocky Mountain Fresh")
     assert_equal "Rocky Mountain Fresh", vendor.name
     assert_equal ({}), vendor.inventory
+  end
+
+  def test_item_stock_starts_at_0
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+
+    assert_equal 0, vendor.check_stock(item1)
+  end
+
+  def test_it_can_stock_item
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    vendor.stock(item1, 30)
+    expected = {
+      item1 => 30
+    }
+    assert_equal expected, vendor.inventory
+  end
+
+  def test_it_can_check_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+
+    vendor.stock(item1, 30)
+
+    assert_equal 30, vendor.check_stock(item1)
+
+    vendor.stock(item1, 25)
+
+    assert_equal 55, vendor.check_stock(item1)
+  end
+
+  def test_it_is_adding_items_to_inventory
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor.stock(item1, 30)
+    vendor.stock(item1, 25)
+    vendor.stock(item2, 12)
+
+    expected = {
+      item1 => 55,
+      item2 => 12
+    }
+
+    assert_equal expected, vendor.inventory
   end
 end
