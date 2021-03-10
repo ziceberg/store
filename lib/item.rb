c91cf695013f8c93262a7fd5004174c19fe3cc15
Zachery Bergman <bzach634@gmail.com>
2021-03-10 09:04:36 -0700

    Add Item class and test_it_exists test

---
+++ lib/item.rb
@@ -0,0 +1,6 @@
+class Item
+  def initialize(info)
+    @name = info[:name]
+    @price = info[:price]
+  end
+end
--- test/item_test.rb
+++ test/item_test.rb
@@ -5,4 +5,8 @@ require 'pry'
 
 
 class ItemTest < Minitest::Test
+  def test_it_exists
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    assert_instance_of Item, item1
+  end
 end
