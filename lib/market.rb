bfc89cfccdc3d70a12ba219e12b2313c422db104
Zachery Bergman <bzach634@gmail.com>
2021-03-10 09:36:00 -0700

    Add market class and test_it_exists

---
+++ lib/market.rb
@@ -0,0 +1,7 @@
+class Market
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+  end
+end
--- test/market_test.rb
+++ test/market_test.rb
@@ -6,4 +6,18 @@ require './lib/vendor'
 require 'pry'
 
 class MarketTest < Minitest::Test
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
 end
